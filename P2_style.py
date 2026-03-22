960f092f1b54e5a9f976cbaf939d747215d73242
673040468-9_Wirithipha <wirithipha.d@kkumail.com>
2026-03-22 22:37:36 +0700

    Add files via upload

---
+++ P2_style.py
@@ -0,0 +1,8 @@
+from PySide6.QtGui import QColor
+
+CONDITION_COLORS = {
+    "Sunny":  QColor(255, 236, 153),
+    "Cloudy": QColor(200, 220, 240),
+    "Rainy":  QColor(153, 204, 255),
+    "Stormy": QColor(180, 150, 210),
+}
